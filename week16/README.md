d79926828f1329d85b7f7a1668d7529e35b7e58e
rogelio-mares <77692713+rogelio-mares@users.noreply.github.com>
2022-05-12 14:11:33 -0600

    Add files via upload

---
+++ week16/README.md
@@ -0,0 +1,3 @@
+For this final project I decided to follow through The Coding Tain Perlin Noise youtube tutorial. I thought it was really cool and thought it would be a good idea to follow and mess around with it and add to it.
+
+https://www.youtube.com/playlist?list=PLRqwX-V7Uu6bgPNQAdxQZpJuJCjeOr7VD
---
+++ week16/js/sketch.js
@@ -0,0 +1,52 @@
+//Varibles
+var inc = 0.1;
+var scl = 10;
+var cols, rows;
+var zoff = 0;
+var fr;
+var particles = [];
+var flowfield;
+
+
+//Canvas
+function setup() {
+  createCanvas(640, 480);
+  colorMode(510);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  fr = createP('');
+
+  flowfield = new Array(cols * rows);
+
+  for (var i = 0; i < 300; i++) {
+    particles[i] = new Particle();
+  }
+  background(0);
+}
+
+function draw() {
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
+      xoff += inc;
+      stroke(0, 50);
+    }
+    yoff += inc;
+
+    zoff += 0.0003;
+  }
+
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+
+}
